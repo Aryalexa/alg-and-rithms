0749d77f5fae5477f820386eae54d463eddf847a
MAlexa <aryalexa@gmail.com>
2023-12-19 20:31:52 +0100

    cc2 step2

--- cc-fyi/2-json_parser/json_parser.py
+++ cc-fyi/2-json_parser/json_parser.py
@@ -41,52 +41,96 @@ class Lexer:
 			self.current_char = self.text[self.pos]
 		else:
 			self.current_char = None
+
+	def skip_whitespace(self):
+		while self.current_char and self.current_char.isspace():
+			self.advance()
+	def string(self):
+		s = ''
+		self.advance()
+		while self.current_char and self.current_char != '"':
+			s += self.current_char
+			self.advance()
+		self.advance()
+		return s
+
 	
-	def get_next_token(self):
+	def tokens(self):
 		while self.current_char:
-			if self.current_char =='{':
+			if self.current_char.isspace():
+				self.skip_whitespace()
+				continue
+			if self.current_char == '{':
 				self.advance()
 				yield Token(TokenType.LBRACE, '{')
-			elif self.current_char =='}':
+			elif self.current_char == '}':
 				self.advance()
 				yield Token(TokenType.RBRACE, '}')
+			elif self.current_char == ':':
+				self.advance()
+				yield Token(TokenType.COLON, ':')
+			elif self.current_char == ',':
+				self.advance()
+				yield Token(TokenType.COMMA, ',')
+			elif self.current_char == '"':
+				yield Token(TokenType.STRING, self.string())
 			else: 
-				raise Exception('')
+				raise Exception(f'bad char {self.current_char}')
 		yield None
 			
 class Parser:
 	def __init__(self, lexer:Lexer) -> None:
 		self.lexer = lexer
-		self.current_token = next(self.lexer.get_next_token())
+		self.current_token = next(self.lexer.tokens())
 
 	def eat(self, token_type:TokenType):
 		if self.current_token and self.current_token.type == token_type:
-			self.current_token = next(self.lexer.get_next_token())
+			self.current_token = next(self.lexer.tokens())
 		else:
 			raise Exception('unexpected token') # current_token vs expected_token
 
-
-
-	def json_dict(self):
-		d:dict = {}
-		print(self.current_token)
-		if self.current_token and self.current_token.type == TokenType.LBRACE:
-			self.eat(TokenType.LBRACE)
+	def parse_key(self):
+		if self.current_token and self.current_token.type == TokenType.STRING:
+			key = self.current_token.value
+			self.eat(TokenType.STRING)
+			return key
 		else:
-			raise Exception("bad start")
-		
-		if self.current_token and self.current_token.type == TokenType.RBRACE:
-			self.eat(TokenType.RBRACE)
+			raise Exception("bad key")
+
+	def parse_value(self):
+		if self.current_token and self.current_token.type == TokenType.STRING:
+			key = self.current_token.value
+			self.eat(TokenType.STRING)
+			return key
 		else:
-			raise Exception("bad end")
-		return d
+			raise Exception("bad value")
+
+	def parse_object(self) -> dict:
+		obj:dict = {}
+		self.eat(TokenType.LBRACE)
+		trailing_coma = False
+		# ---- key:value LOOP
+		while self.current_token and self.current_token.type != TokenType.RBRACE:
+			trailing_coma = False
+			key = self.parse_key()
+			self.eat(TokenType.COLON)
+			value = self.parse_value()
+			obj[key] = value
+			if self.current_token and self.current_token.type == TokenType.COMMA:
+				self.eat(TokenType.COMMA)
+				trailing_coma = True
+		# ----
+		if trailing_coma:
+			raise Exception("trailing comma")
+		self.eat(TokenType.RBRACE)
+		return obj
 		
 
 def json_parser(json_str:str):
 	lexer = Lexer(json_str)
 	parser = Parser(lexer)
 	
-	x = parser.json_dict()
+	x = parser.parse_object()
 	print(x)
 	return x
 	#sys.exit(-1)
--- cc-fyi/2-json_parser/tests/test_parser.py
+++ cc-fyi/2-json_parser/tests/test_parser.py
@@ -6,7 +6,7 @@ def test_step1_invalid():
     file = 'step1/invalid'
     with open(f"tests/tests_files/{file}.json") as f:
         json_str = f.read() 
-    with pytest.raises(Exception, match="bad start"):
+    with pytest.raises(Exception, match="unexpected token"):
         jp.json_parser(json_str)
 
 def test_step1_valid():
@@ -14,3 +14,32 @@ def test_step1_valid():
     with open(f"tests/tests_files/{file}.json") as f:
         json_str = f.read() 
     assert jp.json_parser(json_str) == {}
+
+def test_step2_invalid():
+    file = 'step2/invalid'
+    with open(f"tests/tests_files/{file}.json") as f:
+        json_str = f.read() 
+    with pytest.raises(Exception, match="trailing comma"):
+        jp.json_parser(json_str)
+
+def test_step2_invalid2():
+    file = 'step2/invalid2'
+    with open(f"tests/tests_files/{file}.json") as f:
+        json_str = f.read() 
+    with pytest.raises(Exception, match="bad char k"):
+        jp.json_parser(json_str)
+
+def test_step2_valid():
+    file = 'step2/valid'
+    with open(f"tests/tests_files/{file}.json") as f:
+        json_str = f.read() 
+    assert jp.json_parser(json_str) == {"key":"value"}
+
+def test_step2_valid2():
+    file = 'step2/valid2'
+    with open(f"tests/tests_files/{file}.json") as f:
+        json_str = f.read() 
+    assert jp.json_parser(json_str) == {
+        "key": "value",
+        "key2": "value"
+    }
